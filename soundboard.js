808c33c400a97a424ad88710d14ff3f14bc5766f
Yihao He <heyihao@msu.edu>
2020-11-13 13:19:04 +0800

    Update

--- soundboard.js
+++ soundboard.js
@@ -2,6 +2,14 @@ var buttonBlip = document.getElementById('blip-button')
 var buttonBoom = document.getElementById('boom-button')
 var buttonCoin = document.getElementById('coin-button')
 
+var soundBlip = document.getElementById('blip-sound')
+var soundBoom = document.getElementById('boom-sound')
+var soundCoin = document.getElementById('coin-sound')
+
+function playAudio(audio){
+    new Audio(audio).play()
+}
+
 if(buttonBlip){
     buttonBlip.addEventListener('click',function(){
         console.log('Blip was clicked!')
@@ -28,5 +36,6 @@ if(buttonCoin){
 else {
     "Miss buttonCoin."
 }
+
     
   
